1889d65e4262e655826f7240345bfe79c56d0952
study4 <study4@gradenmall.com.tw>
2022-04-19 03:59:27 +0000

    re init conf of DB & GFB

--- conf/constant.js
+++
@@ -1,9 +0,0 @@
-/**
- * @description 設定項的常數
- */
-
-module.exports = {
-    USER: {
-        AVATAR: 'https://cdn-icons-png.flaticon.com/512/149/149071.png'
-    }
-}
--- firebase/init.js
+++ firebase/init.js
@@ -5,11 +5,11 @@
 const admin = require('firebase-admin');
 const { getStorage } = require('firebase-admin/storage')
 
-const serviceAccount = require('../keys/admin/firebase-4-koa-blog.json');
+const serviceAccount = require('../conf/GFB_admin_key.json');
 
 const app = admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: "fir-4-koa-blog.appspot.com",
+    storageBucket: "gfb20220419.appspot.com",
   });
 
 const storage = getStorage(app)
