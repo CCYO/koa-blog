e2e4c4d4c7e04ca4aa6e13a6b85d392d0258344b
study4 <study4@gmail.com>
2022-10-25 05:55:53 +0000

    g ok

--- routes/views/blog.js
+++ routes/views/blog.js
@@ -24,18 +24,38 @@ router.get('/blog/new', view_check_login, cacheBlog, async (ctx, next) => {
 
 //  編輯文章
 router.get('/blog/edit/:blog_id', view_check_login, cacheBlog, async (ctx, next) => {
+    // const { blog_id } = ctx.params
+    
+    // if (!ctx.blog.length) {
+    //     const resModel = await getBlog(blog_id * 1)
+    //     ctx.blog[1] = resModel.data.blog
+    // }
+    
+    // if (ctx.blog[1].author.id != ctx.session.user.id) {
+    //     return ctx.body = '你哪位?'
+    // }
+    
+    // return await ctx.render('blog-edit', { blog: ctx.blog[1] })
+
     const { blog_id } = ctx.params
-    
-    if (!ctx.blog.length) {
-        const resModel = await getBlog(blog_id * 1)
-        ctx.blog[1] = resModel.data.blog
+    //   = { exist: BOO, kv: [K, V] }
+    let { exist, kv } = ctx.cache
+    if (exist === 3) {
+        const resModel = await getBlog(blog_id, true)
+        ctx.cache.blog = [ undefined, resModel ]
+        console.log(`@blog/${blog_id} 完成 DB撈取`)
+    }else{
+        let [ etag, resModel ] = kv
+        ctx.cache.blog = [ etag, resModel ]
     }
-    
-    if (ctx.blog[1].author.id != ctx.session.user.id) {
+
+    let blog = ctx.cache.blog[1].data
+
+    if (blog.blog.author.id != ctx.session.user.id) {
         return ctx.body = '你哪位?'
     }
-    
-    return await ctx.render('blog-edit', { blog: ctx.blog[1] })
+
+    return await ctx.render('blog', blog)
 })
 
 //  查看文章
